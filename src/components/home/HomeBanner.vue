8b926afc564972e6333b6090ad3c92b2748964c8
Ray <470437155@qq.com>
2019-10-21 18:02:54 +0800

    home-book

--- src/components/home/HomeBanner.vue
+++ src/components/home/HomeBanner.vue
@@ -1,38 +1,64 @@
 <template>
   <div class="home-banner" @click="onClick">
-      <div class="bg-img">
-          <div class="title">{{title}}</div>
-          <div class="subtitle">{{subTitle}}</div>
-      </div>
+    <div class="bg-img" :style="{backgroundImage:bgImage}">
+      <div class="title">{{title}}</div>
+      <div class="subtitle">{{subTitle}}</div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    props:{
-        title:String,
-        subTitle:String,
-        img:String
-    },
-    methods:{
-        onClick(){
-            this.$emit('onClick')
-        }
+  props: {
+    title: String,
+    subTitle: String,
+    img: String
+  },
+  computed:{
+      bgImage(){
+          return 'url('+this.img+')'
+      }
+  },
+  methods: {
+    onClick() {
+      this.$emit("onClick");
     }
-}
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-    .home-banner{
-        width: 100%;
-        height: 100px;
-        margin-top: 20px;
-        padding: 0 20px;
-        box-sizing: border-box;
-        .bg-img{
-            background:yellow;
-            .title{}
-            .subtitle{}
-        }
+.home-banner {
+  width: 100%;
+  margin-top: 20px;
+  padding: 0 20px;
+  box-sizing: border-box;
+  .bg-img {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 125px;
+    border-radius: 10px;
+    background: yellow;
+    background-size: 100% 100%;
+    background-repeat: no-repeat;
+    .title {
+      font-size: 18px;
+      font-weight: 500;
+      color: #fff;
     }
+    .subtitle {
+      padding: 5px 15px;
+      background: #4585ff;
+      border: 1px solid #ccc;
+      border-radius: 50px;
+      margin-top: 10px;
+      font-size: 14px;
+      font-weight: 500;
+      color: #fff;
+    }
+  }
+}
 </style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,7 +2,15 @@
   <div class="home">
     <SearchBar :disabled="true" />
     <HomeCard />
-    <HomeBanner title="来了来了" subTitle="6666666" src />
+    <HomeBanner
+      title="来了来了"
+      subTitle="6666666"
+      img="http://www.youbaobao.xyz/book/res/bg.jpg"
+      @onClick="onBannerClick"
+    />
+    <div :style="{marginTop:'23px'}">
+      <HomeBook title="为你推荐" :data="data" :row="1" :col="3"/>
+    </div>
   </div>
 </template>
 
@@ -10,18 +18,71 @@
 import SearchBar from "../../components/home/SearchBar";
 import HomeCard from "../../components/home/HomeCard";
 import HomeBanner from "../../components/home/HomeBanner";
+import HomeBook from "../../components/home/HomeBook";
 export default {
   data() {
-    return {};
+    return {
+      data: [
+        {
+          id: 225,
+          fileName: "2016_Book_MicrofinanceEUStructuralFundsA",
+          cover:
+            "https://www.youbaobao.xyz/book/res/img/Economics/2016_Book_MicrofinanceEUStructuralFundsA.jpeg",
+          title:
+            "Microfinance, EU Structural Funds and Capacity Building for Managing Authorities",
+          author: "Giovanni Nicola Pes",
+          publisher: "Palgrave Macmillan",
+          bookId: "2016_Book_MicrofinanceEUStructuralFundsA",
+          category: 3,
+          categoryText: "Economics",
+          language: "en",
+          rootFile: "OEBPS/9781137536013.opf"
+        },
+        {
+          id: 88,
+          fileName: "2018_Book_BetweenMobilityAndMigration",
+          cover:
+            "https://www.youbaobao.xyz/book/res/img/SocialSciences/978-3-319-77991-1_CoverFigure.jpg",
+          title: "Between Mobility and Migration",
+          author: "Peter Scholten",
+          publisher: "Springer International Publishing",
+          bookId: "2018_Book_BetweenMobilityAndMigration",
+          category: 2,
+          categoryText: "SocialSciences",
+          language: "en",
+          rootFile: "OEBPS/package.opf"
+        },
+        {
+          id: 24,
+          fileName: "2018_Book_SecurityInComputerAndInformati",
+          cover:
+            "https://www.youbaobao.xyz/book/res/img/ComputerScience/978-3-319-95189-8_CoverFigure.jpg",
+          title: "Security in Computer and Information Sciences",
+          author: "Erol Gelenbe",
+          publisher: "Springer International Publishing",
+          bookId: "2018_Book_SecurityInComputerAndInformati",
+          category: 1,
+          categoryText: "ComputerScience",
+          language: "en",
+          rootFile: "OEBPS/package.opf"
+        }
+      ]
+    };
   },
 
   components: {
     SearchBar,
     HomeCard,
-    HomeBanner
+    HomeBanner,
+    HomeBook
   },
 
-  methods: {},
+  methods: {
+    onSearchBarClick() {},
+    onBannerClick() {
+      console.log("banner click");
+    }
+  },
 
   created() {
     // let app = getApp()
@@ -30,4 +91,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.book-home {
+  margin-top: 23px;
+}
 </style>
